93b9097146051e1091ce046706901c556bca8e68
execbat <evgeniyy.dushkin@gmail.com>
2026-02-22 16:04:08 +0000

    amended heritage of the env class

--- isaaclab_custom_ext/custom_env_5/custom_velocity_env_cfg.py
+++ isaaclab_custom_ext/custom_env_5/custom_velocity_env_cfg.py
@@ -1,4 +1,4 @@
-from isaaclab_tasks.manager_based.locomotion.velocity.velocity_env_cfg import LocomotionVelocityRoughEnvCfg
+from isaaclab.envs import ManagerBasedRLEnvCfg
 from .custom_observations_cfg import ObservationsCfg
 from .custom_rewards_cfg import RewardsCfg
 from .custom_commands_cfg import CommandsCfg
@@ -8,7 +8,10 @@ from .custom_terminations_cfg import TerminationsCfg
 from .custom_curriculum_cfg import CurriculumCfg
 from .custom_actions_cfg import ActionsCfg
 
-class CustomLocomotionVelocityRoughEnvCfg(LocomotionVelocityRoughEnvCfg):
+from isaaclab.utils import configclass
+
+@configclass
+class CustomLocomotionVelocityRoughEnvCfg(ManagerBasedRLEnvCfg):
     scene : SceneCfg = SceneCfg(num_envs=4096, env_spacing=2.5)
     observations: ObservationsCfg = ObservationsCfg()
     rewards:  RewardsCfg = RewardsCfg()
@@ -17,6 +20,22 @@ class CustomLocomotionVelocityRoughEnvCfg(LocomotionVelocityRoughEnvCfg):
     terminations : TerminationsCfg  = TerminationsCfg()
     curriculum: CurriculumCfg = CurriculumCfg()
     actions: ActionsCfg = ActionsCfg()
+    
+    
+    def __post_init__(self):
+        """Post initialization."""
+        # general settings
+        self.decimation = 5  # 20 Hz (because sim.dt = 0.01)
+        
+        self.episode_length_s = 20.0
+        self.is_finite_horizon = False # Send DONE/Truncated siglan to the agent when episode Timeout
+
+        # simulation settings
+        self.sim.dt = 0.01
+        self.sim.render_interval = self.decimation
+        self.sim.physx.bounce_threshold_velocity = 0.01
+        self.sim.physx.gpu_max_rigid_patch_count = 4 * 5 * 2**15   
+             
 
 
 
